3f5cadda604021df847caad6f82c5985e99f9d6d
phantomsmile <47076030+phantomsmile@users.noreply.github.com>
2023-04-02 01:02:26 +0530

    port changed

--- app.js
+++ app.js
@@ -128,7 +128,11 @@ app.get("/:customListName", async (req, res) => {
 app.get("/about", function (req, res) {
   res.render("about");
 });
+let port = process.env.PORT;
+if(port == null || ""){
+  port = 3000;
+}
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
